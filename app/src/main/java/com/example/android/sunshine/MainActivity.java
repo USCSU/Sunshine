1208e5d5e02d2ac554d71c59dcf40656614938a2
Chris Su <chrissuusc@gmail.com>
2015-05-13 16:28:42 -0700

    add button, togglebutton, checkbox and ratingbar to test

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,17 +1,69 @@
 package com.example.android.sunshine;
 
+import android.app.Activity;
+import android.media.Rating;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.example.android.sunshine.R;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView text = (TextView)findViewById(R.id.textview);
+
+        final Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            int count = 0;
+            public void onClick(View v) {
+                button.setText("Got pressed"+ ++count);
+            }
+        });
+        final ToggleButton tb = (ToggleButton)findViewById(R.id.togglebutton);
+        tb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (tb.isChecked()) {
+                    tb.setText("on");
+                } else
+                    tb.setText("off");
+            }
+        });
+        final RatingBar rb = (RatingBar)findViewById(R.id.ratingbar);
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                text.setText("ratings:" + rating);
+            }
+        });
+
+        final CheckBox cb = (CheckBox)findViewById(R.id.checkbox);
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cb.isChecked()) {
+                    cb.setText("I'm checked");
+                } else
+                    cb.setText("I'm not checked");
+            }
+        });
+
+
     }
 
 
